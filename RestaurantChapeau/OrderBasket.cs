a0b867c00af04a582e7d07a8f8c4e3c9a0c3f2fe
Athlon007 <aathlon@outlook.com>
2022-05-18 14:52:56 +0200

    Update OrderBasket.cs

--- RestaurantChapeau/OrderBasket.cs
+++ RestaurantChapeau/OrderBasket.cs
@@ -22,14 +22,11 @@ namespace RestaurantChapeau
         }
 
         // ItemID. Quantity.
-        private Dictionary<int, int> itemsInBasket;
+        private Dictionary<int, int> itemsInBasket = new Dictionary<int, int>();
 
         const int MaximumQuantity = 100;
 
-        private OrderBasket() 
-        {
-            itemsInBasket = new Dictionary<int, int>();
-        }
+        private OrderBasket() { }
 
         /// <summary>
         /// Add ONE of the specified item.
@@ -39,10 +36,12 @@ namespace RestaurantChapeau
         {
             if (itemsInBasket.ContainsKey(item.Id))
             {
+                // Do not allow setting item count above allowed quantity.
                 if (itemsInBasket[item.Id] >= MaximumQuantity)
                 {
                     return;
                 }
+
                 itemsInBasket[item.Id]++;
             }
             else
@@ -63,11 +62,18 @@ namespace RestaurantChapeau
 
                 if (itemsInBasket[item.Id] == 0)
                 {
+                    // Is the item count 0?
+                    // Remove the item from the list.
                     itemsInBasket.Remove(item.Id);
                 }
             }
         }
 
+        /// <summary>
+        /// Sets the item count in the basket to a specific value.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="quantity"></param>
         public void Set(MenuItem item, int quantity)
         {
             if (itemsInBasket.ContainsKey(item.Id))
@@ -81,31 +87,46 @@ namespace RestaurantChapeau
 
             if (itemsInBasket[item.Id] == 0)
             {
+                // Is the item count 0?
+                // Remove the item from the basket.
                 itemsInBasket.Remove(item.Id);
             }
             else if (itemsInBasket[item.Id] > MaximumQuantity)
             {
+                // Is the item count larger than maximum allowed?
+                // Set the count to the max allowed.
                 itemsInBasket[item.Id] = MaximumQuantity;
             }
         }
 
+        /// <summary>
+        /// Returns how many times specific item is in the basket.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public int ItemCount(MenuItem item)
         {
             if (itemsInBasket.ContainsKey(item.Id))
             {
                 return itemsInBasket[item.Id];
             }
-            else
-            {
-                return 0;
-            }
+
+            // If the item is not in the basket, return 0.
+            return 0;
         }
 
+        /// <summary>
+        /// Clears the basket.
+        /// </summary>
         public void Clear()
         {
             itemsInBasket.Clear();
         }
 
+        /// <summary>
+        /// Returns all items in the basket.
+        /// </summary>
+        /// <returns></returns>
         public Dictionary<int, int> GetAll()
         {
             return itemsInBasket;
