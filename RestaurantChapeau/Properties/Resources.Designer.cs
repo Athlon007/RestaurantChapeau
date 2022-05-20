3579ce576c9e0e797736808cc15d4a6addad2579
hohyunLee <683070@student.inholland.nl>
2022-05-20 15:44:35 +0200

    I created table view panel

--- RestaurantChapeau/Properties/Resources.Designer.cs
+++ RestaurantChapeau/Properties/Resources.Designer.cs
@@ -79,5 +79,25 @@ namespace RestaurantChapeau.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap screenshotTable {
+            get {
+                object obj = ResourceManager.GetObject("screenshotTable", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap table5 {
+            get {
+                object obj = ResourceManager.GetObject("table5", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
--- RestaurantChapeau/TableViewForm.cs
+++ RestaurantChapeau/TableViewForm.cs
@@ -14,5 +14,76 @@ namespace RestaurantChapeau
         {
             InitializeComponent();
         }
+        private void TableViewForm_Load(object sender, EventArgs e)
+        {
+            HidePanel();
+        }
+        public void HidePanel()
+        {
+            pnl_TableViews.Hide();
+        }             
+        private void btn_Table1_Click(object sender, EventArgs e)
+        {
+            HidePanel();
+            pnl_TableViews.Show();
+        }
+        private void btn_Table2_Click(object sender, EventArgs e)
+        {
+            HidePanel();
+            pnl_TableViews.Show();
+        }        
+        private void btn_TableViewPnGoBack_Click(object sender, EventArgs e)
+        {
+            HidePanel();
+            TableViewForm_Load(sender, e);
+        }
+
+        private void btn_Table3_Click(object sender, EventArgs e)
+        {
+            HidePanel();
+            pnl_TableViews.Show();
+        }
+
+        private void btn_Table4_Click(object sender, EventArgs e)
+        {
+            HidePanel();
+            pnl_TableViews.Show();
+        }
+
+        private void btn_Table5_Click(object sender, EventArgs e)
+        {
+            HidePanel();
+            pnl_TableViews.Show();
+        }
+
+        private void btn_Table6_Click(object sender, EventArgs e)
+        {
+            HidePanel();
+            pnl_TableViews.Show();
+        }
+
+        private void btn_Table7_Click(object sender, EventArgs e)
+        {
+            HidePanel();
+            pnl_TableViews.Show();
+        }
+
+        private void btn_Table8_Click(object sender, EventArgs e)
+        {
+            HidePanel();
+            pnl_TableViews.Show();
+        }
+
+        private void btn_Table9_Click(object sender, EventArgs e)
+        {
+            HidePanel();
+            pnl_TableViews.Show();
+        }
+
+        private void btn_Table10_Click(object sender, EventArgs e)
+        {
+            HidePanel();
+            pnl_TableViews.Show();
+        }
     }
 }
